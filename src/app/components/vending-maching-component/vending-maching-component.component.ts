6b06edffba3835dac9b1663c469932c3dadc30b8
jamesXFY <45582531+jamesXFY@users.noreply.github.com>
2020-01-20 00:25:42 +1000

    change initial data

--- src/app/components/vending-maching-component/vending-maching-component.component.ts
+++ src/app/components/vending-maching-component/vending-maching-component.component.ts
@@ -17,43 +17,83 @@ export class VendingMachingComponentComponent implements OnInit {
 
   constructor() {
     const can1: Can = {
-      type: 'test',
-      name: 'test',
+      type: 'item1',
+      name: 'item1',
       price: 12.33,
-      amount: 20,
+      amount: 1,
       icon: 'icon (1).svg'
     };
 
     const can2: Can = {
-      type: 'test1',
-      name: 'test1',
+      type: 'item2',
+      name: 'item2',
       price: 12.33,
-      amount: 10,
+      amount: 2,
       icon: 'icon (2).svg'
     };
 
     const can3: Can = {
-      type: 'test3',
-      name: 'test3',
+      type: 'item3',
+      name: 'item3',
       price: 12.33,
-      amount: 10,
+      amount: 3,
       icon: 'icon (3).svg'
     };
 
     const can4: Can = {
-      type: 'test4',
-      name: 'test4',
+      type: 'item4',
+      name: 'item4',
       price: 12.33,
-      amount: 10,
+      amount: 4,
       icon: 'icon (4).svg'
     };
 
     const can5: Can = {
-      type: 'test4',
-      name: 'test4',
+      type: 'item5',
+      name: 'item5',
+      price: 12.33,
+      amount: 5,
+      icon: 'icon (5).svg'
+    };
+
+    const can6: Can = {
+      type: 'item6',
+      name: 'item6',
+      price: 12.33,
+      amount: 6,
+      icon: 'icon (6).svg'
+    };
+
+    const can7: Can = {
+      type: 'item7',
+      name: 'item7',
+      price: 12.33,
+      amount: 7,
+      icon: 'icon (7).svg'
+    };
+
+    const can8: Can = {
+      type: 'item8',
+      name: 'item8',
+      price: 12.33,
+      amount: 8,
+      icon: 'icon (8).svg'
+    };
+
+    const can9: Can = {
+      type: 'item9',
+      name: 'item9',
+      price: 12.33,
+      amount: 9,
+      icon: 'icon (9).svg'
+    };
+
+    const can10: Can = {
+      type: 'item10',
+      name: 'item10',
       price: 12.33,
       amount: 10,
-      icon: 'icon (5).svg'
+      icon: 'icon (10).svg'
     };
 
     this.cans.push(can1);
@@ -61,6 +101,11 @@ export class VendingMachingComponentComponent implements OnInit {
     this.cans.push(can3);
     this.cans.push(can4);
     this.cans.push(can5);
+    this.cans.push(can6);
+    this.cans.push(can7);
+    this.cans.push(can8);
+    this.cans.push(can9);
+    this.cans.push(can10);
    }
 
   ngOnInit() {
